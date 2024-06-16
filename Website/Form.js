ce25dad454e8ed7e87d1741b4bbbac435eae09b7
DJC5 <cates9559@gmail.com>
2024-06-15 21:19:21 -0500

    Create Form.js

---
+++ Website/Form.js
@@ -0,0 +1,8 @@
+// inline comments are for the sake of the website
+/* multiline comments are for the sake of the website 
+ghthtrhtr*/
+
+var bean = "Beans";
+let cheese = "Cheese";
+const onion = "Onion";
+console.log(bean + cheese +onion);
